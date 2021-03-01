739b78aa44bd64875a9c3f7e42695e8b1135b818
Nicholas Markus <nicxzmiller2@gmail.com>
2021-03-01 10:10:03 +0100

    channels

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDJCiLEfwLCtPqgWobQq_MunQYTnI98ubg",
     authDomain: "slack-clone-yt-18918.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "373252479157",
     appId: "1:373252479157:web:a4431e046f76ead14046fc"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
